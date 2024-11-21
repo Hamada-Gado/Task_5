aad869e17286a51693f90a6cc0e390f5fe814f55
Ahmed Gado <ahmedgado0100@gmail.com>
2024-11-21 09:06:38 +0200

    finish task 5

--- src/tests/getusers.test.js
+++ src/tests/getusers.test.js
@@ -4,32 +4,46 @@ const app = require("../App");
 
 /* Connecting to the database before each test. */
 beforeEach(async () => {
-    await mongoose.connect(process.env.MONGO_URI);
-  });
+  await mongoose.connect(process.env.MONGO_URI);
+});
 
- /* Closing database connection after each test. */
- afterEach(async  () => {
+/* Closing database connection after each test. */
+afterEach(async () => {
   await mongoose.connection.close();
-
-});   
+});
 /* Test for getting  user by id. */
 describe("GET users/:id", () => {
-    test("Get user by Id", (done) => {
-      request(app)
-        .get("/users/67388414be4de6f8bbe4477b")
-        .expect(200)
-        .expect((res) => {
-         expect(res.body.name).toBe("John Doe");
-        })
-        .end((err, res) => {
-          if (err) return done(err);
-          return done();
-        });
-    });
+  test("Get user by Id", (done) => {
+    request(app)
+      .get("/users/673dd6c489e9547e55dfc4d0")
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.name).toBe("John Doe");
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
   });
-
+});
 
 /* Test for creating a new user. */
-
-          
-
+describe("POST /signup", () => {
+  test("Create a new user", (done) => {
+    request(app)
+      .post("/signup")
+      .send({
+        name: "John Doe",
+        email: "johndoe@gmail.com",
+        password: "password",
+      })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.name).toBe("John Doe");
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
